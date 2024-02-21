cc65326b1f7843e2bda01fd69d4cbabd378ade92
Bn4 <bnfour@bnfour.net>
2024-02-21 17:33:13 +0700

    hidden cursor mod: implemented

--- HiddenCursor/HiddenCursorMod.cs
+++ HiddenCursor/HiddenCursorMod.cs
@@ -1,8 +1,15 @@
 using MelonLoader;
+using UnityEngine;
 
 namespace Bnfour.MusynxMods.HiddenCursor;
 
 public class HiddenCursorMod : MelonMod
 {
-    
+    public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+    {
+        base.OnSceneWasLoaded(buildIndex, sceneName);
+
+        var isGameScene = sceneName.StartsWith("UI") && sceneName.EndsWith("_PlayingScene");
+        Cursor.visible = !isGameScene;
+    }
 }
---
+++ HiddenCursor/Patches/VariousSongPlayersPlayResumePlayPausePatch.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Bnfour.MusynxMods.HiddenCursor.Patches;
+
+// both methods are only used by Harmony itself, not by anything in the assembly
+#pragma warning disable IDE0051
+
+// that's a ridiculous name, but at least it's accurate-ish
+/// <summary>
+/// Handles game (un)pausing: shows the cursor when the game is paused,
+/// hides it again on unpausing.
+/// </summary>
+[HarmonyPatch]
+public class VariousSongPlayersPlayResumePlayPausePatch
+{
+    private static IEnumerable<MethodBase> TargetMethods()
+    {
+        // basically, this patch is for both PlayPause and PlayResume methods
+        // of all the UI<whatever>_SongPlayer classes, which seem to be not related
+
+        return AccessTools.GetTypesFromAssembly(typeof(UI0_SongPlayer).Assembly)
+            .Where(type => type.Name.StartsWith("UI") && type.Name.EndsWith("_SongPlayer"))
+            .SelectMany(type => type.GetMethods())
+            .Where(method => method.Name.StartsWith("Play") && method.ReturnType == typeof(void))
+            .Cast<MethodBase>();
+    }
+
+    private static void Postfix(MethodBase __originalMethod)
+    {
+        // PlayPause may return early without pausing the game,
+        // notably before and after the actual song;
+        // the easiest way to check if the game was actually paused across all
+        // the different *_SongPlayer classes is to check the timescale
+        // -- it's set to 0 when the game is paused
+        var actuallyPaused = __originalMethod.Name == "PlayPause" && Time.timeScale == 0f;
+
+        Cursor.visible = actuallyPaused;
+    }
+}
+
+#pragma warning restore
