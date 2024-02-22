e314c1139d4ab2e4b2ba307a40ae27a1046a4d88
Bn4 <bnfour@bnfour.net>
2024-02-22 17:21:23 +0700

    vsync annihilator: first incomplete pass (and a lot of playtesting)
    the new fps cap is hardcoded 240 (obviously because my display is 240 Hz), i also tested 600 (also seems to work) and unlimited (timings at 1500+ fps seem to break)

---
+++ VSyncAnnihilator/Patches/SetterIgnorerPatch.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+
+namespace Bnfour.MusynxMods.VSyncAnnihilator.Patches;
+
+// both methods are only used by Harmony itself, not by anything in the assembly
+#pragma warning disable IDE0051
+
+/// <summary>
+/// Patch that denies any changes to properties specified,
+/// unless they come from within this mod.
+/// </summary>
+[HarmonyPatch]
+public class SetterIgnorerPatch
+{
+    private static IEnumerable<MethodBase> TargetMethods()
+    {
+        // there are some assignments to targetFrameRate of 60 and vSyncCount of 1 in the code,
+        // and we want to ignore these to keep custom setings
+        yield return AccessTools.PropertySetter(typeof(Application), nameof(Application.targetFrameRate));
+        yield return AccessTools.PropertySetter(typeof(QualitySettings), nameof(QualitySettings.vSyncCount));
+    }
+
+    private static bool Prefix()
+    {
+        return Melon<VSyncAnnihilatorMod>.Instance.ApproveChange;
+    }
+}
+
+#pragma warning restore
--- VSyncAnnihilator/VSyncAnnihilatorMod.cs
+++ VSyncAnnihilator/VSyncAnnihilatorMod.cs
@@ -1,8 +1,37 @@
+using System;
 using MelonLoader;
+using UnityEngine;
 
 namespace Bnfour.MusynxMods.VSyncAnnihilator;
 
 public class VSyncAnnihilatorMod : MelonMod
 {
-    
+
+    public bool ApproveChange { get; private set; }
+
+    public override void OnInitializeMelon()
+    {
+        base.OnInitializeMelon();
+        // TODO make framerate configurable
+        Sudo(() => Application.targetFrameRate = 240);
+
+        if (QualitySettings.vSyncCount != 0)
+        {
+            LoggerInstance.Warning("Selected settings preset has vSync on! Did you patch a preset and choose it?");
+            LoggerInstance.Warning("Disabling now, can cause lags.");
+            Sudo(() => QualitySettings.vSyncCount = 0);
+        }
+    }
+
+    /// <summary>
+    /// Performs the action with a guard patch disabled.
+    /// </summary>
+    /// <param name="action">Action to perform, should be either
+    /// Application.targetFrameRate or QualitySettings.vSyncCount change.</param>
+    private void Sudo(Action action)
+    {
+        ApproveChange = true;
+        action.Invoke();
+        ApproveChange = false;
+    }
 }
