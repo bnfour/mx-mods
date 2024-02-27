fd19f1d38089e662161e52a4af06188c205a86f4
Bn4 <bnfour@bnfour.net>
2024-02-27 21:55:29 +0700

    plentiful stats: feature toggles for every feature planned

--- PlentifulStats/PlentifulStatsMod.cs
+++ PlentifulStats/PlentifulStatsMod.cs
@@ -4,5 +4,31 @@ namespace Bnfour.MusynxMods.PlentifulStats;
 
 public class PlentifulStatsMod : MelonMod
 {
+    private MelonPreferences_Category _prefsCategory;
+    private MelonPreferences_Entry<bool> _rToRestartEnabled;
+    private MelonPreferences_Entry<bool> _separateExactsEnabled;
+    private MelonPreferences_Entry<bool> _prevBestEnabled;
+    // values to check in patches
+    internal bool RToRestart => _rToRestartEnabled.Value;
+    internal bool SeparateExacts => _separateExactsEnabled.Value;
+    internal bool PrevBest => _prevBestEnabled.Value;
 
+    public override void OnInitializeMelon()
+    {
+        base.OnInitializeMelon();
+
+        _prefsCategory = MelonPreferences.CreateCategory("Bnfour_PlentifulStats");
+
+        _rToRestartEnabled = _prefsCategory.CreateEntry("RToRestart", true,
+            "Enable R to restart", "Enables R key to restart from the stats screen.");
+        _separateExactsEnabled = _prefsCategory.CreateEntry("SeparateExacts", true,
+            "Separate exacts count", "Displays separate blue and cyan exacts counts.");
+        _prevBestEnabled = _prefsCategory.CreateEntry("PrevBest", true,
+            "Previous best", "Displays previous best score at the stats screen.");
+
+        if (!RToRestart && !SeparateExacts && !PrevBest)
+        {
+            LoggerInstance.Warning("No features of the mod enabled, it can be unistalled.");
+        }
+    }
 }
