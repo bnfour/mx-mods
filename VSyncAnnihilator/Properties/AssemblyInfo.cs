8677fd8dacbe603b7bdd4147cc6770964e42e2a3
Bn4 <bnfour@bnfour.net>
2024-02-28 18:17:06 +0700

    all mods version bumped to 1.0.0 in anticipation of the firest release

--- VSyncAnnihilator/Properties/AssemblyInfo.cs
+++ VSyncAnnihilator/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using MelonLoader;
 
 using Bnfour.MusynxMods.VSyncAnnihilator;
 
-[assembly: MelonInfo(typeof(VSyncAnnihilatorMod), "VSync annihilator", "0.0.1", "bnfour", "https://github.com/bnfour/mx-mods")]
+[assembly: MelonInfo(typeof(VSyncAnnihilatorMod), "VSync annihilator", "1.0.0", "bnfour", "https://github.com/bnfour/mx-mods")]
 [assembly: MelonGame("coweye", "MUSYNX")]
 [assembly: MelonColor(255, 202, 80, 16)]
 [assembly: MelonAuthorColor(255, 128, 128, 128)]
