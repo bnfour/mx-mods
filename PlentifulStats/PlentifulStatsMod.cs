56e2d455e528b9dfa6705ea2f46b5e6c61c4d5eb
Bn4 <bnfour@bnfour.net>
2024-03-16 20:12:45 +0700

    plentiful stats: an option for an upcoming feature
    it's a fix for the "NEXT" button turning "继续" for scores greater than 120 (red EX and beyond) due to an unfortunate oversight
    
    it's more of a bugfix than a mod

--- PlentifulStats/PlentifulStatsMod.cs
+++ PlentifulStats/PlentifulStatsMod.cs
@@ -8,10 +8,12 @@ public class PlentifulStatsMod : MelonMod
     private MelonPreferences_Entry<bool> _rToRestartEnabled;
     private MelonPreferences_Entry<bool> _separateExactsEnabled;
     private MelonPreferences_Entry<bool> _prevBestEnabled;
+    private MelonPreferences_Entry<bool> _nextButtonFixEnabled;
     // values to check in patches
     internal bool RToRestart => _rToRestartEnabled.Value;
     internal bool SeparateExacts => _separateExactsEnabled.Value;
     internal bool PrevBest => _prevBestEnabled.Value;
+    internal bool EnNextFix => _nextButtonFixEnabled.Value;
 
     /// <summary>
     /// Stores the previous high score for a song to display on the stats screen
@@ -37,8 +39,10 @@ public class PlentifulStatsMod : MelonMod
             "Separate exacts count", "Displays separate blue and cyan exacts counts.");
         _prevBestEnabled = _prefsCategory.CreateEntry("PrevBest", true,
             "Previous best", "Displays previous best score at the stats screen.");
+        _nextButtonFixEnabled = _prefsCategory.CreateEntry("EnNextFix", true,
+            "Next button fix", "Fixes Next button switching to Chinese on 120+ scores.");
 
-        if (!RToRestart && !SeparateExacts && !PrevBest)
+        if (!RToRestart && !SeparateExacts && !PrevBest && !EnNextFix)
         {
             LoggerInstance.Warning("No features of the mod enabled, it can be unistalled.");
         }
