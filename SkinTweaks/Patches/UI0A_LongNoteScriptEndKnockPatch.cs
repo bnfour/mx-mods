1f6839c370d4c2f7a74c73df44988833799e49ed
Bn4 <bnfour@bnfour.net>
2024-07-29 01:01:18 +0700

    skin tweaks -- long notes: skeletons of patches to re-calculate the score and update the ui with necessary checks
    re-calculation of the score and updating the ui coming soon™

---
+++ SkinTweaks/Patches/UI0A_LongNoteScriptEndKnockPatch.cs
@@ -0,0 +1,40 @@
+using HarmonyLib;
+using MelonLoader;
+
+namespace Bnfour.MusynxMods.SkinTweaks.Patches;
+
+[HarmonyPatch(typeof(UI0A_LongNoteScript), "EndKnock")]
+public class UI0A_LongNoteScriptEndKnockPatch
+{
+    // this method is called a lot of times,
+    // but only does some work once when UI0A_LongNoteScript.endKnock is false,
+    // setting it to true in the process
+
+    // so in order to this patch to match this behaviour,
+    // we need to monitor the value of that flag as well
+
+    private static void Prefix(UI0A_LongNoteScript __instance, out bool __state)
+    {
+        if (Melon<SkinTweaksMod>.Instance.LongNotesEndScoring)
+        {
+            __state = Traverse.Create(__instance).Field<bool>("endKnock").Value;
+        }
+        else
+        {
+            // tell postfix to do nothing
+            __state = true;
+        }
+    }
+
+    private static void Postfix(UI0A_LongNoteScript __instance, bool __state, bool ___knocking)
+    {
+        // whether the feature is enabled at all is checked by prefix,
+        // __state being true means the actual method did nothing and/or this feature is disabled;
+        // UI0A_LongNoteScript.knocking is also checked: if it's false, the note was dropped mid-way,
+        // and another patch already updated the UI with relevant score value
+        if (!__state && ___knocking)
+        {
+            // soon™
+        }
+    }
+}
---
+++ SkinTweaks/Patches/UI0A_LongNoteScriptLiftUpPatch.cs
@@ -0,0 +1,22 @@
+using HarmonyLib;
+using MelonLoader;
+
+namespace Bnfour.MusynxMods.SkinTweaks.Patches;
+
+[HarmonyPatch(typeof(UI0A_LongNoteScript), nameof(UI0A_LongNoteScript.LiftUp))]
+public class UI0A_LongNoteScriptLiftUpPatch
+{
+    // LiftUp method is called once when a long note was played, but is dropped
+    // before its actual end, so some of the combo will end up missing
+    
+    // this will reset UI0A_LongNoteScript.knocking flag -- we use this fact
+    // in another patch
+
+    private static void Postfix(UI0A_LongNoteScript __instance)
+    {
+        if (Melon<SkinTweaksMod>.Instance.LongNotesEndScoring)
+        {
+            // soon™
+        }
+    }
+}
