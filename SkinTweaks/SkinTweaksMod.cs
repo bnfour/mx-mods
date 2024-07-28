9a2c11cfefb77ea94fb4071c94d666653c7e8629
Bn4 <bnfour@bnfour.net>
2024-07-28 22:43:26 +0700

    skin tweaks -- long notes: option to control implemented
    (i'm doing the two tweaks in parallel to have a merge conflict -- it's been a long time since i saw one)
    
    this is going to be a feature to update the score on the skin with built-in realtime score display whenever the long note ends

--- SkinTweaks/SkinTweaksMod.cs
+++ SkinTweaks/SkinTweaksMod.cs
@@ -5,11 +5,17 @@ namespace Bnfour.MusynxMods.SkinTweaks;
 public class SkinTweaksMod : MelonMod
 {
     private MelonPreferences_Category _prefsCategory;
+    private MelonPreferences_Entry<bool> _longNotesEndScoringEnabled;
+
+    internal bool LongNotesEndScoring => _longNotesEndScoringEnabled.Value;
 
     public override void OnInitializeMelon()
     {
         base.OnInitializeMelon();
 
         _prefsCategory = MelonPreferences.CreateCategory("Bnfour_SkinTweaks");
+
+        _longNotesEndScoringEnabled = _prefsCategory.CreateEntry("LongNoteEndScoring", true,
+            "Update display score on long note end", "Updates the score display on UI0A when long notes end.");
     }
 }
