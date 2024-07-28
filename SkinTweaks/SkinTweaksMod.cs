e66d93f628d80256d0a0bbb135cbeff591f82ea2
Bn4 <bnfour@bnfour.net>
2024-07-28 22:30:15 +0700

    skin tweaks: base settings category
    to be utilized in branches for individual tweaks

--- SkinTweaks/SkinTweaksMod.cs
+++ SkinTweaks/SkinTweaksMod.cs
@@ -4,5 +4,12 @@ namespace Bnfour.MusynxMods.SkinTweaks;
 
 public class SkinTweaksMod : MelonMod
 {
+    private MelonPreferences_Category _prefsCategory;
 
+    public override void OnInitializeMelon()
+    {
+        base.OnInitializeMelon();
+
+        _prefsCategory = MelonPreferences.CreateCategory("Bnfour_SkinTweaks");
+    }
 }
