944a6cd35434270e650a8f978d258355b0087d7b
Bn4 <bnfour@bnfour.net>
2025-01-12 23:58:10 +0700

    menu tweaks -- ordinals: an actuall on/off toggle in the config file

--- MenuTweaks/MenuTweaksMod.cs
+++ MenuTweaks/MenuTweaksMod.cs
@@ -5,13 +5,16 @@ namespace Bnfour.MusynxMods.MenuTweaks;
 public class MenuTweaksMod : MelonMod
 {
     private MelonPreferences_Category _prefsCategory;
+    private MelonPreferences_Entry<bool> _ordinalsFixEnabled;
 
-    internal bool OrdinalFixEnabled => true;
+    internal bool OrdinalFixEnabled => _ordinalsFixEnabled.Value;
 
     public override void OnInitializeMelon()
     {
         base.OnInitializeMelon();
 
         _prefsCategory = MelonPreferences.CreateCategory("Bnfour_MenuTweaks");
+        _ordinalsFixEnabled = _prefsCategory.CreateEntry("OrdinalsFix", true,
+            "Ordinals fix", "Fixes suffixes for your rank in song selection menus.");
     }
 }
