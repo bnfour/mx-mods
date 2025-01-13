c6168a178f102591ba2f997243edea1163deb70e
Bn4 <bnfour@bnfour.net>
2025-01-13 21:12:06 +0700

    menu tweaks: version changed to 1.0.0 following completion unless i forgot something again

--- MenuTweaks/Properties/AssemblyInfo.cs
+++ MenuTweaks/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using MelonLoader;
 
 using Bnfour.MusynxMods.MenuTweaks;
 
-[assembly: MelonInfo(typeof(MenuTweaksMod), "Menu tweaks", "0.0.1", "bnfour", "https://github.com/bnfour/mx-mods")]
+[assembly: MelonInfo(typeof(MenuTweaksMod), "Menu tweaks", "1.0.0", "bnfour", "https://github.com/bnfour/mx-mods")]
 [assembly: MelonGame("coweye", "MUSYNX")]
 [assembly: MelonColor(255, 202, 80, 16)]
 [assembly: MelonAuthorColor(255, 128, 128, 128)]
