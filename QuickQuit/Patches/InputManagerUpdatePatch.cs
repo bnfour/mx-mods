04dcf8612664e1b6377a7636b7d8accbcce906c2
Bn4 <bnfour@bnfour.net>
2025-04-13 20:07:45 +0700

    quick quit: patches to init and update custom input manager with the original one
    yeah, the mod's full name is "quick quit and restart", but i'll just
    call it "quick quit" for that fancy QQ abbreviation

---
+++ QuickQuit/Patches/InputManagerUpdatePatch.cs
@@ -0,0 +1,17 @@
+using HarmonyLib;
+using MelonLoader;
+
+namespace Bnfour.MusynxMods.QuickQuit.Patches;
+
+/// <summary>
+/// A patch that just calls the similar update method for the custom input
+/// whenever the original InputManager updates.
+/// </summary>
+[HarmonyPatch(typeof(InputManager), "Update")]
+public class InputManagerUpdatePatch
+{
+    private static void Postfix()
+    {
+        Melon<QuickQuitMod>.Instance.ExtraInput.Update();
+    }
+}
