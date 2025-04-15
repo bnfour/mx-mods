8307bb6fcca4b324601bb99399c10b30c7369d27
Bn4 <bnfour@bnfour.net>
2025-04-15 14:51:42 +0700

    quick quit: version bumped to 1.0.0 in preparation for a release (where this mod's appearance is the only notable thing)

--- QuickQuit/Properties/AssemblyInfo.cs
+++ QuickQuit/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using MelonLoader;
 
 using Bnfour.MusynxMods.QuickQuit;
 
-[assembly: MelonInfo(typeof(QuickQuitMod), "Quick quit and restart", "0.0.1", "bnfour", "https://github.com/bnfour/mx-mods")]
+[assembly: MelonInfo(typeof(QuickQuitMod), "Quick quit and restart", "1.0.0", "bnfour", "https://github.com/bnfour/mx-mods")]
 [assembly: MelonGame("coweye", "MUSYNX")]
 [assembly: MelonColor(255, 202, 80, 16)]
 [assembly: MelonAuthorColor(255, 128, 128, 128)]
