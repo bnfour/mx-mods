a1ea92723fdfefd38d8df3bf4fc15eddbef83860
Bn4 <bnfour@bnfour.net>
2025-06-30 22:33:29 +0700

    song info: data poco to be used for caching info as opposed to loading resources everty time and an outline of the patch that does nothing for now

---
+++ SongInfo/Data/SongData.cs
@@ -0,0 +1,23 @@
+namespace Bnfour.MusynxMods.SongInfo.Data;
+
+/// <summary>
+/// Data structure to hold cached data about a song.
+/// </summary>
+public class SongData
+{
+    /// <summary>
+    /// Song's BPM, preformatted as a string. If BPM changes during the map,
+    /// this a minimum and maximum value separated by "~", like "1~999"
+    /// </summary>
+    public string Bpm { get; set; }
+
+    /// <summary>
+    /// Song duration, preformatted to "mm:ss" like "01:34".
+    /// </summary>
+    public string Duration { get; set; }
+
+    /// <summary>
+    /// Indicates if the map has non-BPM scroll speed changes.
+    /// </summary>
+    public bool HasSv { get; set; }
+}
---
+++ SongInfo/Patches/SelectSongControllerSongChangesPatch.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+
+namespace Bnfour.MusynxMods.SongInfo.Patches;
+
+/// <summary>
+/// A patch that is fired on song or difficulty change to update the custom UI.
+/// </summary>
+[HarmonyPatch]
+internal class SelectSongControllerSongChangesPatch
+{
+    // saved for reuse, the original method is not directly accessible
+    private static readonly MethodInfo _coreByIndex = typeof(SelectSongController).GetMethod("GetSongInfoCoreByIndex", BindingFlags.Instance | BindingFlags.NonPublic);
+
+    internal static IEnumerable<MethodBase> TargetMethods()
+    {
+        // called when the "big" menu changes songs/difficulties
+        yield return AccessTools.Method(typeof(SelectSongController), "ChangingSongBg");
+        // called when the list menu changes songs/difficulties, seems to be called twice in a row when moving down
+        yield return AccessTools.Method(typeof(SelectSongController), "ChangeShowSmall");
+    }
+
+    internal static void Postfix(SelectSongController __instance, MethodBase __originalMethod)
+    {
+        var songInfoCore = _coreByIndex.Invoke(__instance, [__instance.songIndex]);
+
+        // TODO get data from cache or data by songInfoCore
+        // SongInfoCore -> SongData? (returns null when data is n/a, like shop entry in the big menu)
+
+        // update UI (which one is determined by method name)
+        // maybe in a helper class as well?
+        var isBigMenu = __originalMethod.Name.Equals("ChangingSongBg");
+    }
+}
